673d51608ac584511c5546641dfdf4fb112ee77b
jjuny7712 <jjuny7712@gmail.com>
2023-07-17 17:28:14 +0900

    init: generate project

---
+++ moyeolam-be/src/test/java/com/ssafy/moyeolam/MoyeolamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.moyeolam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeolamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
