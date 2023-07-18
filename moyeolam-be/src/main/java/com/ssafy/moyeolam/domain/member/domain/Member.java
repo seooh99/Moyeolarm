5c5c97ad0938edb21162377841349b5c17026209
이용준 <jjuny7712@gmail.com>
2023-07-18 14:27:50 +0900

    feat: 모든 도메인에 대한 Base 엔티티 클래스 생성

---
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/member/domain/Member.java
@@ -0,0 +1,37 @@
+package com.ssafy.moyeolam.domain.member.domain;
+
+import com.ssafy.moyeolam.domain.BaseTimeEntity;
+import com.ssafy.moyeolam.domain.friend.domain.Friend;
+import com.ssafy.moyeolam.domain.meta.domain.OauthType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "member")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OauthType oauthType;
+
+    @Column
+    private String oauthIdentifier;
+
+    @Column
+    private String nickname;
+
+    @OneToMany(mappedBy = "member")
+    private List<Friend> friends = new ArrayList<>();
+}
