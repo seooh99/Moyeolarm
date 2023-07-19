aefcbe07f04b45aa9f94691cd82787fa9073d9f9
jjuny7712 <jjuny7712@gmail.com>
2023-07-19 17:58:33 +0900

    feat: 메타데이터 관련 Service 생성자 추가

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/meta/domain/MetaDataType.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/meta/domain/MetaDataType.java
@@ -1,5 +1,5 @@
 package com.ssafy.moyeolam.domain.meta.domain;
 
 public enum MetaDataType {
-    AlarmGroupMemberRole, AlarmMission, AlarmSound, AlertType, DayOfWeek, MatchStatus, OauthType, PushAlertType
+    ALARM_GROUP_MEMBER_ROLE, ALARM_MISSION, ALARM_SOUND, ALERT_TYPE, DAY_OF_WEEK, MATCH_STATUS, OAUTH_TYPE, PUSH_ALERT_TYPE
 }
--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/meta/service/MetaDataService.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/meta/service/MetaDataService.java
@@ -1,7 +1,6 @@
 package com.ssafy.moyeolam.domain.meta.service;
 
-import com.ssafy.moyeolam.domain.meta.domain.MetaData;
-import com.ssafy.moyeolam.domain.meta.domain.MetaDataProvider;
+import com.ssafy.moyeolam.domain.meta.domain.*;
 import com.ssafy.moyeolam.global.common.exception.GlobalErrorInfo;
 import com.ssafy.moyeolam.global.common.exception.GlobalException;
 import org.springframework.stereotype.Component;
@@ -17,10 +16,19 @@ public class MetaDataService {
 
     public MetaDataService() {
         this.map = new HashMap<>();
+        this.putMetaData(MetaDataType.ALARM_GROUP_MEMBER_ROLE.name(), AlarmGroupMemberRole.class);
+        this.putMetaData(MetaDataType.ALARM_MISSION.name(), AlarmMission.class);
+        this.putMetaData(MetaDataType.ALARM_SOUND.name(), AlarmSound.class);
+        this.putMetaData(MetaDataType.ALERT_TYPE.name(), AlertType.class);
+        this.putMetaData(MetaDataType.DAY_OF_WEEK.name(), DayOfWeek.class);
+        this.putMetaData(MetaDataType.MATCH_STATUS.name(), MatchStatus.class);
+        this.putMetaData(MetaDataType.OAUTH_TYPE.name(), OauthType.class);
+        this.putMetaData(MetaDataType.PUSH_ALERT_TYPE.name(), PushAlertType.class);
     }
 
     public MetaData getMetaData(String type, String name) {
-        MetaData metaData = this.getMetaMap(type.toLowerCase()).get(name.toLowerCase());
+        MetaData metaData = this.getMetaMap(type.toLowerCase())
+                .get(name.toLowerCase());
 
         if (metaData == null) {
             throw new GlobalException(GlobalErrorInfo.NOT_FOUND);
