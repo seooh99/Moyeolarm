1eb9cad4c8df61a1405d8bb2ac593152623b5cf2
jjuny7712 <jjuny7712@gmail.com>
2023-07-20 10:26:59 +0900

    feat: 알람그룹 생성 Service 구현

---
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/service/AlarmGroupService.java
@@ -0,0 +1,30 @@
+package com.ssafy.moyeolam.domain.alarmgroup.service;
+
+import com.ssafy.moyeolam.domain.alarmgroup.domain.AlarmGroup;
+import com.ssafy.moyeolam.domain.alarmgroup.dto.AlarmGroupGenerateRequest;
+import com.ssafy.moyeolam.domain.alarmgroup.repository.AlarmGroupRepository;
+import com.ssafy.moyeolam.domain.meta.domain.MetaDataType;
+import com.ssafy.moyeolam.domain.meta.service.MetaDataService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AlarmGroupService {
+    private final MetaDataService metaDataService;
+    private final AlarmGroupRepository alarmGroupRepository;
+
+    public Long generateAlarmGroup(AlarmGroupGenerateRequest request) {
+        AlarmGroup alarmGroup = AlarmGroup.builder()
+                .time(request.getTime())
+                .repeat(request.getDayOfWeek().size() > 0)
+                .alarmSound(metaDataService.getMetaData(MetaDataType.ALARM_SOUND.name(), request.getAlarmSound()))
+                .alarmMission(metaDataService.getMetaData(MetaDataType.ALARM_MISSION.name(), request.getAlarmMission()))
+                .build();
+
+        alarmGroupRepository.save(alarmGroup);
+        return alarmGroup.getId();
+    }
+}
