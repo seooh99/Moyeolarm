61252f8ab952a6c5d045d64e876c886165848573
jjuny7712 <jjuny7712@gmail.com>
2023-07-20 15:25:06 +0900

    feat: 알람그룹 목록 조회 Service 구현

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/service/AlarmGroupService.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/service/AlarmGroupService.java
@@ -1,24 +1,34 @@
 package com.ssafy.moyeolam.domain.alarmgroup.service;
 
 import com.ssafy.moyeolam.domain.alarmgroup.domain.AlarmGroup;
+import com.ssafy.moyeolam.domain.alarmgroup.dto.FindAlarmGroupsResponseDto;
 import com.ssafy.moyeolam.domain.alarmgroup.dto.SaveAlarmGroupRequestDto;
 import com.ssafy.moyeolam.domain.alarmgroup.repository.AlarmGroupRepository;
+import com.ssafy.moyeolam.domain.member.domain.Member;
+import com.ssafy.moyeolam.domain.member.repository.MemberRepository;
 import com.ssafy.moyeolam.domain.meta.domain.MetaDataType;
 import com.ssafy.moyeolam.domain.meta.service.MetaDataService;
+import com.ssafy.moyeolam.global.common.exception.GlobalErrorInfo;
+import com.ssafy.moyeolam.global.common.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AlarmGroupService {
     private final MetaDataService metaDataService;
+    private final MemberRepository memberRepository;
     private final AlarmGroupRepository alarmGroupRepository;
 
+
     @Transactional
-    public Long generateAlarmGroup(SaveAlarmGroupRequestDto requestDto) {
+    public Long generateAlarmGroup(SaveAlarmGroupRequestDto requestDto, Long loginMemberId) {
         /**
          * TODO: 사용자 및 권한 검증 로직 추가
          */
@@ -33,6 +43,18 @@ public class AlarmGroupService {
 
         alarmGroupRepository.save(alarmGroup);
         return alarmGroup.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public FindAlarmGroupsResponseDto findAlarmGroups(Long loginMemberId) {
+        /**
+         * memberException으로 변경
+         */
+        Member loginMember = memberRepository.findById(loginMemberId)
+                .orElseThrow(() -> new GlobalException(GlobalErrorInfo.INTERNAL_SERVER_ERROR));
+
+        List<AlarmGroup> alarmGroups = alarmGroupRepository.findAllWithAlarmGroupMembersFetch();
+        return FindAlarmGroupsResponseDto.of(alarmGroups, loginMember);
     }
 
 }
