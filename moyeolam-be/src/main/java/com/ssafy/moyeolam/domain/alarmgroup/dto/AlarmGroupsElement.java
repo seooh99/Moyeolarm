a991af7ba332881a8487535dcb04d18737f69617
이용준 <jjuny7712@gmail.com>
2023-07-21 11:09:32 +0900

    feat: 알람그룹 단건 조회 기능 구현

---
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/dto/AlarmGroupsElement.java
@@ -0,0 +1,35 @@
+package com.ssafy.moyeolam.domain.alarmgroup.dto;
+
+import com.ssafy.moyeolam.domain.alarmgroup.domain.AlarmGroup;
+import com.ssafy.moyeolam.domain.alarmgroup.domain.AlarmGroupMember;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class AlarmGroupsElement {
+    private Long alarmGroupId;
+    private String title;
+    private LocalTime time;
+    private List<String> dayOfWeek;
+    private Boolean toggle;
+
+    public static AlarmGroupsElement of(AlarmGroupMember alarmGroupMember) {
+        AlarmGroup alarmGroup = alarmGroupMember.getAlarmGroup();
+
+        return AlarmGroupsElement.builder()
+                .alarmGroupId(alarmGroup.getId())
+                .title(alarmGroup.getTitle())
+                .time(alarmGroup.getTime())
+                .dayOfWeek(alarmGroup.getAlarmDays()
+                        .stream()
+                        .map(day -> day.getDayOfWeek().getName())
+                        .collect(Collectors.toList()))
+                .toggle(alarmGroupMember.getAlarmToggle())
+                .build();
+    }
+}
