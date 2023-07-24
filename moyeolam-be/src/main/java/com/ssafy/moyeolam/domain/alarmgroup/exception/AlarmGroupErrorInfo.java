6f8b2cc40c4905b03b6e2ef6808ba938d2ad612a
jjuny7712 <jjuny7712@gmail.com>
2023-07-24 17:32:42 +0900

    fix: 알람그룹 나가기 시 알람그룹 요청 매칭상태 갱신

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/exception/AlarmGroupErrorInfo.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/exception/AlarmGroupErrorInfo.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum AlarmGroupErrorInfo {
-    UNAUTHORIZED_ACCESS("801", "알람그룹 접근 권한이 없습니다."),
+    ALREADY_NOT_ALARM_GROUP_MEMBER("801", "이미 알람그룹 멤버가 아닙니다."),
     NOT_FOUND_ALARM_GROUP("802", "존재하지 않는 알람그룹입니다."),
     UNAUTHORIZED_UPDATE("803", "알람그룹 수정 권한이 없습니다."),
     UNAUTHORIZED_REQUEST("804", "알람그룹 초대 권한이 없습니다."),
-    NOT_FOUND_ALARM_GROUP_REQUEST("805", "잘못된 알람그룹 요청입니다."),
+    NOT_FOUND_ALARM_GROUP_REQUEST("805", "알람 그룹 요청이 없습니다."),
     UNAUTHORIZED_APPROVE("806", "알람그룹 수락 권한이 없습니다."),
     UNAUTHORIZED_BAN("807", "알람그룹 강퇴 권한이 없습니다."),
     NOT_SELF_BAN("808", "자신은 강퇴할 수 없습니다."),
--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/service/AlarmGroupService.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/service/AlarmGroupService.java
@@ -99,7 +99,7 @@ public class AlarmGroupService {
                 .orElseThrow(() -> new AlarmGroupException(AlarmGroupErrorInfo.NOT_FOUND_ALARM_GROUP));
 
         if (!alarmGroupMemberRepository.existsByMemberIdAndAlarmGroupId(loginMember.getId(), alarmGroupId)) {
-            throw new AlarmGroupException(AlarmGroupErrorInfo.UNAUTHORIZED_ACCESS);
+            throw new AlarmGroupException(AlarmGroupErrorInfo.ALREADY_NOT_ALARM_GROUP_MEMBER);
         }
 
         return FindAlarmGroupResponseDto.of(alarmGroup, loginMember);
@@ -114,18 +114,21 @@ public class AlarmGroupService {
                 .orElseThrow(() -> new AlarmGroupException(AlarmGroupErrorInfo.NOT_FOUND_ALARM_GROUP));
 
         if (!alarmGroupMemberRepository.existsByMemberIdAndAlarmGroupId(loginMember.getId(), alarmGroupId)) {
-            throw new AlarmGroupException(AlarmGroupErrorInfo.UNAUTHORIZED_ACCESS);
+            throw new AlarmGroupException(AlarmGroupErrorInfo.ALREADY_NOT_ALARM_GROUP_MEMBER);
         }
 
-        boolean isHost = alarmGroup.getHostMember().getId().equals(loginMember.getId());
-        if (isHost) {
+        Member hostMember = alarmGroup.getHostMember();
+        if (hostMember.getId().equals(loginMember.getId())) {
             alarmGroupMemberRepository.deleteAllInBatch(alarmGroup.getAlarmGroupMembers());
             alarmDayRepository.deleteAllInBatch(alarmGroup.getAlarmDays());
             alarmGroupRepository.delete(alarmGroup);
             return alarmGroup.getId();
         }
-
         alarmGroupMemberRepository.deleteByMemberIdAndAlarmGroupId(loginMember.getId(), alarmGroupId);
+
+        AlarmGroupRequest alarmGroupRequest = alarmGroupRequestRepository.findByAlarmGroupIdAndFromMemberIdAndToMemberId(alarmGroup.getId(), hostMember.getId(), loginMember.getId())
+                .orElseThrow(() -> new AlarmGroupException(AlarmGroupErrorInfo.NOT_FOUND_ALARM_GROUP_REQUEST));
+        alarmGroupRequest.setMatchStatus(metaDataService.getMetaData(MetaDataType.MATCH_STATUS.name(), MatchStatus.DELETE_STATUS.getName()));
 
         AlertLog alertLog = AlertLog.builder()
                 .fromMember(loginMember)
