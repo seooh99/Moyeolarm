55536be1667d708b35326ad31cdab6b2e95da60e
jjuny7712 <jjuny7712@gmail.com>
2023-07-24 15:44:37 +0900

    feat: 알람그룹 강퇴 관련 예외 메시지 추가

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/exception/AlarmGroupErrorInfo.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/alarmgroup/exception/AlarmGroupErrorInfo.java
@@ -9,7 +9,10 @@ public enum AlarmGroupErrorInfo {
     UNAUTHORIZED_UPDATE("803", "알람그룹 수정 권한이 없습니다."),
     UNAUTHORIZED_REQUEST("804", "알람그룹 초대 권한이 없습니다."),
     NOT_FOUND_ALARM_GROUP_REQUEST("805", "잘못된 알람그룹 요청입니다."),
-    UNAUTHORIZED_APPROVE("806", "알람그룹 수락 권한이 없습니다.");
+    UNAUTHORIZED_APPROVE("806", "알람그룹 수락 권한이 없습니다."),
+    UNAUTHORIZED_BAN("807", "알람그룹 강퇴 권한이 없습니다."),
+    NOT_SELF_BAN("808", "자신은 강퇴할 수 없습니다."),
+    NOT_FOUND_ALARM_GROUP_MEMBER("809", "알람그룹 멤버를 찾을 수 없습니다.");
 
 
     private String code;
