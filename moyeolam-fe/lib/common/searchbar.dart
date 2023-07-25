2fb17f429d9b88f9a725d61194d54cbc7fa343f4
임서희 <seooh212@gmail.com>
2023-07-25 09:45:58 +0900

    feat: searchbar 구현 수정완료와 외 3개

---
+++ moyeolam-fe/lib/common/searchbar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:youngjun/common/const/colors.dart';
+
+
+class PinnedSearchBarApp extends StatefulWidget {
+  const PinnedSearchBarApp({super.key});
+
+  @override
+  State<PinnedSearchBarApp> createState() => _PinnedSearchBarAppState();
+}
+
+class _PinnedSearchBarAppState extends State<PinnedSearchBarApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: MAIN_COLOR),
+        home: Scaffold(
+          body: SafeArea(
+            child: CustomScrollView(
+              slivers: <Widget>[
+
+                SliverAppBar(
+
+                  clipBehavior: Clip.none,
+                  shape: const StadiumBorder(),
+                  scrolledUnderElevation: 0.0,
+                  titleSpacing: 0.0,
+                  backgroundColor: BACKGROUND_COLOR,
+                  floating:
+                  true, // We can also uncomment this line and set `pinned` to true to see a pinned search bar.
+                  title: SearchAnchor.bar(
+                    suggestionsBuilder:
+                        (BuildContext context, SearchController controller) {
+                      return List<Widget>.generate(
+                        5,
+                            (int index) {
+                          return ListTile(
+                            titleAlignment: ListTileTitleAlignment.center,
+                            title: Text('Initial list item $index'),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+                // The listed items below are just for filling the screen
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
