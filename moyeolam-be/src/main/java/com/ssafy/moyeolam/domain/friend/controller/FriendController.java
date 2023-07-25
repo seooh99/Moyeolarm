c4201da9ec3f1bc8756f176346b61fcd84eeb226
profornnan <profornnan@naver.com>
2023-07-25 09:20:21 +0900

    feat: findFriends Controller 구현

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/controller/FriendController.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/controller/FriendController.java
@@ -1,5 +1,6 @@
 package com.ssafy.moyeolam.domain.friend.controller;
 
+import com.ssafy.moyeolam.domain.friend.dto.FindFriendsResponseDto;
 import com.ssafy.moyeolam.domain.friend.service.FriendService;
 import com.ssafy.moyeolam.domain.member.dto.AuthenticatedMember;
 import com.ssafy.moyeolam.global.common.response.EnvelopeResponse;
@@ -33,6 +34,17 @@ public class FriendController {
 
         return EnvelopeResponse.<Void>builder()
                 .data(friendService.approveFriendRequest(loginMember.getMemberId(), friendRequestId))
+                .build();
+    }
+
+    @GetMapping
+    public EnvelopeResponse<FindFriendsResponseDto> findFriends() {
+        AuthenticatedMember loginMember = AuthenticatedMember.builder()
+                .memberId(1L)
+                .build();
+
+        return EnvelopeResponse.<FindFriendsResponseDto>builder()
+                .data(friendService.findFriends(loginMember.getMemberId()))
                 .build();
     }
 }
