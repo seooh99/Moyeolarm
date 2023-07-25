3b1d525eaaae06137b3277b33608e119d1ed86b9
profornnan <profornnan@naver.com>
2023-07-25 13:11:40 +0900

    feat: findByMemberIdAndMyFriendId 구현

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/repository/FriendRepository.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/repository/FriendRepository.java
@@ -4,6 +4,9 @@ import com.ssafy.moyeolam.domain.friend.domain.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    Optional<Friend> findByMemberIdAndMyFriendId(Long memberId, Long myFriendId);
 }
