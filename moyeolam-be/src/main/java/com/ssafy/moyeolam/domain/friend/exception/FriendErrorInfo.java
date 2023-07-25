f85525b62abbefca1ea6d88689d0b9f06c57a54c
profornnan <profornnan@naver.com>
2023-07-25 13:10:43 +0900

    feat: NOT_FRIEND_STATUS FriendErrorInfo 추가

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/exception/FriendErrorInfo.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/friend/exception/FriendErrorInfo.java
@@ -8,7 +8,8 @@ public enum FriendErrorInfo {
     ALREADY_APPROVE_STATUS("702", "이미 친구인 상태입니다."),
     NOT_FOUND_FRIEND_REQUEST("703", "존재하지 않는 친구 요청입니다."),
     NOT_REQUESTED_USER("704", "사용자에게 온 친구 요청이 아닙니다."),
-    NOT_REQUEST_STATUS("705", "친구 요청 상태가 아닙니다.");
+    NOT_REQUEST_STATUS("705", "친구 요청 상태가 아닙니다."),
+    NOT_FRIEND_STATUS("706", "친구 상태가 아닙니다.");
 
     private String code;
     private String message;
