057a5e8eca8b38141bf1b989b61a0dcb84b9e65a
강성구 <sungku2757@gmail.com>
2023-07-25 15:00:50 +0900

    fix: clock ui 크기 설정
    * fix: clock ui 크기 설정

--- moyeolam-fe/lib/common/clock.dart
+++ moyeolam-fe/lib/common/clock.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/cupertino.dart';
 
-class DatePickerExample extends StatefulWidget {
-  const DatePickerExample({super.key});
+class Clock extends StatefulWidget {
+  const Clock({super.key});
 
   @override
-  State<DatePickerExample> createState() => _DatePickerExampleState();
+  State<Clock> createState() => ClockState();
 }
-class _DatePickerExampleState extends State<DatePickerExample> {
+class ClockState extends State<Clock> {
   DateTime time = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 22, 35);
 
   @override
   Widget build(BuildContext context) {
     return
-      CupertinoDatePicker(
-      initialDateTime: time,
-      mode: CupertinoDatePickerMode.time,
-      // use24hFormat: true,
-      // minimumDate: DateTime.now()
-      // This is called when the user changes the time.
-      onDateTimeChanged: (DateTime newTime) {
-        var today_hour = DateTime.now().hour;
-        var today_minute = DateTime.now().minute;
-        var today_second = DateTime.now().second;
-        if(newTime.hour < today_hour);
-        else if(newTime.hour == today_hour);
-        // else if();
-        setState(() => time = newTime);
-        print(time);
-      },
+      Container(
+        height: 150,
+
+        child: CupertinoDatePicker(
+        initialDateTime: time,
+        mode: CupertinoDatePickerMode.time,
+        // use24hFormat: true,
+        // minimumDate: DateTime.now()
+        // This is called when the user changes the time.
+        onDateTimeChanged: (DateTime newTime) {
+          setState(() => time = newTime);
+          print(time);
+        },
+        ),
       );
   }
 }
