18682d6b020d7d308a34a26e262c47ee72670e62
강성구 <sungku2757@gmail.com>
2023-07-27 13:35:43 +0900

    feat: kakao login logic 및 provider 저장
    * feat: kakao login logic 및 provider 저장

--- moyeolam-fe/lib/user/view/set_nickname.dart
+++ moyeolam-fe/lib/user/view/set_nickname.dart
@@ -3,40 +3,35 @@ import 'package:youngjun/common/const/colors.dart';
 import 'package:youngjun/common/textfield_bar.dart';
 import 'package:youngjun/common/button/btn_back.dart';
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+class SetNickname extends StatelessWidget {
+  const SetNickname({super.key});
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             BtnBack(
-              onPressed: () =>
-                Navigator.pop(context),
+              onPressed: () => Navigator.pop(context),
             ),
             const SizedBox(
               width: 200,
               height: 120,
             ),
             Container(
-              child: TextFieldbox(
-
-              ),
+              child: TextFieldbox(),
             ),
             const SizedBox(
               width: 200,
               height: 360,
             ),
             Center(
-              child:  ElevatedButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text("시작하기"),
-              )
-            ),
+                child: ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("시작하기"),
+            )),
           ],
-
         ),
         backgroundColor: BACKGROUND_COLOR,
       ),
