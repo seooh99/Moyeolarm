32ce41052025c017d309cdf15a6a7e92e140f6e1
허재 <huhwo00@gmail.com>
2023-07-27 20:29:02 +0900

    feat: ëSuccessHandler/FailuerHandler

---
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.ssafy.moyeolam.domain.auth.oauth2.handler;
+
+import com.ssafy.moyeolam.domain.auth.dto.PrincipalDetails;
+import com.ssafy.moyeolam.domain.auth.service.jwt.JwtProvider.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        PrincipalDetails principalDetails =(PrincipalDetails)authentication.getPrincipal();
+        loginSuccess(response, principalDetails);
+        response.sendRedirect("/");
+    }
+
+    private void loginSuccess(HttpServletResponse response, PrincipalDetails principalDetails) throws IOException {
+
+        String accessToken = jwtProvider.createAccessToken(principalDetails.getUsername());
+        String refreshToken = jwtProvider.createRefreshToken();
+        response.addHeader(jwtProvider.getAccessHeader(), "Bearer " + accessToken);
+        response.addHeader(jwtProvider.getRefreshHeader(), "Bearer " + refreshToken);
+
+        jwtProvider.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        jwtProvider.updateRefreshToken(principalDetails.getUsername(), refreshToken);
+
+    }
+}
