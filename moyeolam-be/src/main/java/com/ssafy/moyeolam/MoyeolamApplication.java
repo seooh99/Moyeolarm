be1de60317472819ca5b1c1d912323217acf9524
이용준 <jjuny7712@gmail.com>
2023-08-01 17:43:23 +0900

    feat: 푸시알림 구현
    * feat: 푸시알림이 필요한 기능에 푸시알림 메시지 전송 로직 추가
    
    * feat: 푸시알림 관련 예외 처리 추가
    
    * feat: 푸시알림 관련 Service 구현
    
    * feat: 푸시알림 관련 예외 추가
    
    * chore: 파이어베이스 의존성 추가
    
    * feat: firebaseMessaging Bean 등록

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/MoyeolamApplication.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/MoyeolamApplication.java
@@ -1,16 +1,35 @@
 package com.ssafy.moyeolam;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class MoyeolamApplication {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoyeolamApplication.class, args);
-	}
+@EnableJpaAuditing
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class MoyeolamApplication {
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
+                new ClassPathResource("firebase-service-account.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "moyeolam");
+        return FirebaseMessaging.getInstance(app);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyeolamApplication.class, args);
+    }
 
 }
