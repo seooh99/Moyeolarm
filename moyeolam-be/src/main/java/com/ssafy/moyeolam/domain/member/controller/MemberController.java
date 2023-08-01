0a8837fc2c81737e27d32aa0bf8501e3e3a41aca
허재 <huhwo00@gmail.com>
2023-08-01 23:08:34 +0900

    chore: Remove unnecessary logs and import statements

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/member/controller/MemberController.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/member/controller/MemberController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
