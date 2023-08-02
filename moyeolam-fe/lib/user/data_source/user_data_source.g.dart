8e11a604ec6318f8e599dcb0d7d059891668d5e7
임서희 <seooh212@gmail.com>
2023-08-03 07:59:54 +0900

    fix: 병합충돌해결

--- moyeolam-fe/lib/user/data_source/user_data_source.g.dart
+++ moyeolam-fe/lib/user/data_source/user_data_source.g.dart
@@ -36,11 +36,9 @@ class _UserDataSource implements UserDataSource {
               queryParameters: queryParameters,
               data: _data,
             )
-            .copyWith(
-                baseUrl: _combineBaseUrls(
-              _dio.options.baseUrl,
-              baseUrl,
-            ))));
+
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+
     final value = User.fromJson(_result.data!);
     return value;
   }
@@ -56,22 +54,5 @@ class _UserDataSource implements UserDataSource {
       }
     }
     return requestOptions;
-  }
-
-  String _combineBaseUrls(
-    String dioBaseUrl,
-    String? baseUrl,
-  ) {
-    if (baseUrl == null || baseUrl.trim().isEmpty) {
-      return dioBaseUrl;
-    }
-
-    final url = Uri.parse(baseUrl);
-
-    if (url.isAbsolute) {
-      return url.toString();
-    }
-
-    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
