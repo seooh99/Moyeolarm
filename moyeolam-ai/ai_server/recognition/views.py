7b06bb951fdb076ee22727fffe69ecb5757d654a
jjuny7712 <jjuny7712@gmail.com>
2023-08-03 16:14:08 +0900

    feat: 얼굴 인식 API 서버 구현

--- moyeolam-ai/ai_server/recognition/views.py
+++ moyeolam-ai/ai_server/recognition/views.py
@@ -2,29 +2,63 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import FaceRecognitionSerializer
 
+import mediapipe as mp
+import cv2
+from PIL import Image
+import numpy as np
+
+mp_face_detection = mp.solutions.face_detection
+
+CONFIDENCE_THRESHOLD = 0.8
+
 
 @api_view(['GET'])
 def face(request):
-    image_file = request.FILES["image"]
+    try:
+        image_file = Image.open(request.FILES['image'])
+        image_file = cv2.cvtColor(np.array(image_file), cv2.COLOR_RGB2BGR)
 
-    if image_file is not None:
-        with open('uploaded_image.png', 'wb') as f:
-            for chunk in image_file.chunks():
-                f.write(chunk)
+        confidence = 0
+        recognition_result = False
+        with mp_face_detection.FaceDetection(
+                model_selection=1, min_detection_confidence=0.9) as face_detection:
 
-    response = FaceRecognitionSerializer(data={
-        "code": 200,
-        "message": "success",
-        "data": True
-    })
+            image_file = cv2.cvtColor(image_file, cv2.COLOR_BGR2RGB)
+            results = face_detection.process(image_file)
 
-    if response.is_valid():
-        return Response(response.data)
+            if results.detections:
+                for detection in results.detections:
+                    confidence = max(confidence, detection.score[0])
 
+        if confidence >= CONFIDENCE_THRESHOLD:
+            recognition_result = True
 
-    error_response = FaceRecognitionSerializer(data={
-        "code": 400,
-        "message": "얼굴 인식 처리 도중 오류가 발생",
-        "data": False
-    })
-    return Response(error_response.data)
+        response = FaceRecognitionSerializer(data={
+            "code": 200,
+            "message": "success",
+            "data": {
+                "result": recognition_result,
+                "confidence": confidence * 100
+            }
+        })
+
+        if response.is_valid():
+            return Response(response.data)
+        else:
+            return Response({
+                "code": 400,
+                "message": response.errors,
+                "data": {
+                    "result": False,
+                    "confidence": 0
+                }
+            }, status=400)
+    except:
+        return Response({
+            "code": 400,
+            "message": "얼굴 인식에 오류가 발생했습니다.",
+            "data": {
+                "result": False,
+                "confidence": 0
+            }
+        }, status=400)
