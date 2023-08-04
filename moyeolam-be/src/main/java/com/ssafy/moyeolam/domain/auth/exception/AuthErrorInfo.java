93969dc69e4222b2a65bd55afdab15f93ecd5ed3
허재 <huhwo00@naver.com>
2023-08-04 14:37:20 +0900

    feat: login api login from front end
    * feat: AuthException
    
    * rename: LoginResponseDto
    
    * feat: SecurityConfig-OAuth2Login().disable()
    
    * feat: LoginResponseDto
    
    * feat: LoginRequestDto
    
    * feat: AuthService_login
    
    * feat: AuthController_login

---
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/exception/AuthErrorInfo.java
@@ -0,0 +1,13 @@
+package com.ssafy.moyeolam.domain.auth.exception;
+
+public enum AuthErrorInfo {
+    NOT_FOUND_OAUTH_IDENTIFIER("604", "oauthIdentifier가 없습니다.");
+
+    private String code;
+    private String message;
+
+    AuthErrorInfo(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
