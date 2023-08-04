e253b033a2017ff5469f579e437cd3ab166149e1
허재 <huhwo00@gmail.com>
2023-08-04 14:12:52 +0900

    feat: AuthController_login

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/controller/AuthController.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.ssafy.moyeolam.domain.auth.controller;
 
 import com.ssafy.moyeolam.domain.auth.dto.GetMemberDataResponseDto;
+import com.ssafy.moyeolam.domain.auth.dto.LoginRequestDto;
 import com.ssafy.moyeolam.domain.auth.dto.PrincipalDetails;
 import com.ssafy.moyeolam.domain.auth.service.AuthService;
 import com.ssafy.moyeolam.domain.member.domain.Member;
@@ -8,6 +9,8 @@ import com.ssafy.moyeolam.global.common.response.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,13 +19,24 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping("/login/success")
-    public EnvelopeResponse<GetMemberDataResponseDto> getMemberData(@AuthenticationPrincipal PrincipalDetails principal){
+    @PostMapping("/login")
+    public EnvelopeResponse<GetMemberDataResponseDto> login(LoginRequestDto loginRequestDto){
 
-        Member member = principal.getMember();
+        String oauthIdentifier = loginRequestDto.getOauthIdentifier();
 
         return EnvelopeResponse.<GetMemberDataResponseDto>builder()
-                .data(authService.getMemberData(member))
+                .data(authService.login(oauthIdentifier))
                 .build();
     }
+
+    //    @GetMapping("/login/success")
+//    public EnvelopeResponse<GetMemberDataResponseDto> getMemberData(@AuthenticationPrincipal PrincipalDetails principal){
+//
+//        Member member = principal.getMember();
+//
+//        return EnvelopeResponse.<GetMemberDataResponseDto>builder()
+//                .data(authService.getMemberData(member))
+//                .build();
+//    }
+
 }
