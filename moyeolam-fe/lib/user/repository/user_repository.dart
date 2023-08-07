88ec3ec9c2b441d39ffe108444195187aa49be12
강성구 <sungku2757@gmail.com>
2023-08-07 19:23:59 +0900

    fix: 카카오 로그인 후 닉네임 설정

--- moyeolam-fe/lib/user/repository/user_repository.dart
+++ moyeolam-fe/lib/user/repository/user_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 import 'package:youngjun/common/const/address_config.dart';
-
+import 'package:youngjun/user/model/nickname_model.dart';
 import '../data_source/user_data_source.dart';
 import 'package:youngjun/user/model/user_model.dart';
 
@@ -11,19 +11,27 @@ class UserRepository {
     baseUrl: BASE_URL,
   );
 
-  Future<Map<String, UserModel>> isSigned(String request) {
+  Future<ResponseUserModel> isSigned(String request) {
     print("$request repository");
     IsSigned params = IsSigned(oauthIdentifier: request);
     // print(params.oauthIdentifier);
     return _userDataSource.isSigned(params);
   }
+
+  Future<NicknameResposne> signOut(String token) {
+    print("sign out user repository");
+    return _userDataSource.signOut('Bearer $token');
+  }
 }
 
-class UserNicknameRepository{
+class UserNicknameRepository {
   final UserDataSource _dataSource = UserDataSource(Dio(), baseUrl: BASE_URL);
 
-  Future<Map<String, dynamic>> updateNickname(String newNickname){
+  Future<NicknameResposne> updateNickname(String newNickname, String token) {
     NicknamePost params = NicknamePost(nickname: newNickname);
-    return _dataSource.updateNickname(params);
+    return _dataSource.updateNickname(
+      params,
+      'Bearer $token',
+    );
   }
 }
