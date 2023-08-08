92a91e81590b9cd8dbfc8f31bf99f6faf8f2b1f3
임서희 <seooh212@gmail.com>
2023-08-08 18:55:44 +0900

    Feature/setting push state #159
    * fix: code modify
    
    * feature: setting-push state toggle fcm 반영(미완)

--- moyeolam-fe/lib/fcm/api/fcm_api.dart
+++ moyeolam-fe/lib/fcm/api/fcm_api.dart
@@ -54,29 +54,25 @@ Future<void> initLocalNotifications(BuildContext context) async {
       final parsedJson = jsonDecode(payload);
       if (parsedJson.containsKey('routeTo') && parsedJson['routeTo'] == '/arlet_list') {
         // 알림을 눌렀을 때 '/arlet_list'로 이동하는 로직 추가
-        // Navigator.push(context, MaterialPageRoute(builder: (context) => ArletListView()));
+
       }
     },
   );
 
   final platform = _localNotification.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
   await platform?.createNotificationChannel(AndroidNotificationChannel(
-    'my_notification_channel', // 'default_notification_channel_id'와 일치해야 함
-    'My Notification Channel', // 적절한 채널 이름
-    importance: Importance.high, // 중요도 설정
+    'my_notification_channel',
+    'My Notification Channel',
+    importance: Importance.high,
   ));
 }
 
 
-// @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // 이 함수는 FCM 메시지를 백그라운드에서 처리하는 핸들러입니다.
-  // 백그라운드에서 FCM 메시지를 수신할 때 호출되며, 수신한 메시지의 타이틀과 내용을 출력합니다.
   print('Title: ${message.notification?.title}');
   print('Body: ${message.notification?.body}');
   print(message.notification?.body);
   initPushNotifications();
-  // return handleMessage(message);
 
 }
 
@@ -111,10 +107,6 @@ Future<void> initPushNotifications() async {
 
 
 
-
-
-
-// ChangeNotifier를 상속하고 있으며, FCM 관련 기능을 제공하는 Provider 클래스입니다. FCM을 초기화하고, 토큰을 얻어오며, 백그라운드 메시지 핸들러를 등록하고, 수신한 메시지를 저장하는 등의 기능이 포함되어 있습니다.
 class FirebaseApi with ChangeNotifier {
   final _firebaseMessaging = FirebaseMessaging.instance;
   // String? fcmToken;
@@ -127,25 +119,15 @@ class FirebaseApi with ChangeNotifier {
     final fcmToken = await _firebaseMessaging.getToken();
     print('Token:  $fcmToken');
     FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-    // FirebaseMessaging.instance.onTokenRefresh.listen((newToken) async {
-    //   // save token to server
-    // });
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      // 여기에 특정 페이지로 이동하는 로직을 정의
-      // Navigator.of(context).push(
-      //   MaterialPageRoute(builder: (context) => YourTargetPage()),
-      // );
 
       latestMessage = message; // 메시지 저장
       notifyListeners(); // 변경 알림
     });
-
-    // FirebaseMessaging.instance.deleteToken();
 
     notifyListeners();
     initPushNotifications();
     initLocalNotifications();
   }
 
-// Further FCM methods can go here
 }
