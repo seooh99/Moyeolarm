3aca08b1b3bd7b46ba9a657d23757f30c78cb1e5
jjuny7712 <jjuny7712@gmail.com>
2023-08-08 09:54:55 +0900

    feat: 알람목록 출력 프로바이더 구현

--- moyeolam-fe/lib/alarm/provider/alarm_list_provider.dart
+++ moyeolam-fe/lib/alarm/provider/alarm_list_provider.dart
@@ -1,21 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youngjun/alarm/model/alarm_list_model.dart';
-import 'package:youngjun/alarm/repository/alarm_list_repository.dart';
-import 'package:youngjun/common/const/address_config.dart';
 
-// Dio dio = Dio();
-// AlarmListRepository _alarmListRepository = AlarmListRepository(dio ,baseUrl: BASE_URL);
+import '../repository/alarm_list_repository.dart';
 
-final dioProvider = Provider((ref) => Dio()); // Create a Dio instance
+final alarmListProvider = FutureProvider<List<AlarmGroups>?>((ref) async {
+  final dio = Dio();
+  final alarms = AlarmListRepository(dio);
 
-final _alarmListRepositoryProvider = Provider((ref) {
-  final dio = ref.read(dioProvider);
-  return AlarmListRepository(dio);
-});
-
-final alarmListProvider = FutureProvider<AlarmListModel>((ref) async {
-  final apiService = ref.read(_alarmListRepositoryProvider);
-  final response = await apiService.getAlarmList();
-  return response;
+  return await alarms.getAlarmList().then((value) => value.data.alarmGroups);
 });
