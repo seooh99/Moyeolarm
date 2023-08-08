79195fa330842e63838167f7ffdcc29c3f47e9a4
임서희 <seooh212@gmail.com>
2023-08-08 22:46:43 +0900

    fix: logic 수정(미완)
    * fix: logic 수정(미완)

--- moyeolam-fe/lib/fcm/view/arlet_list_view.dart
+++ moyeolam-fe/lib/fcm/view/arlet_list_view.dart
@@ -4,32 +4,12 @@ import '../../common/layout/title_bar.dart';
 
 import 'package:dio/dio.dart';
 
+import '../model/arlet_service_model.dart';
 import '../service/fcm_api_service.dart';
 import 'package:youngjun/common/const/colors.dart';
 
-// void main() async {
-//   final dio = Dio(); // Dio 인스턴스 생성
-//   final fcmapiService = FcmApiService(dio);
-//
-//   try {
-//     final alerts = await fcmapiService.getPosts(); // API로부터 데이터 가져오기
-//     // alerts 리스트는 API에서 받아온 알림 데이터의 리스트입니다.
-//
-//     for (var alert in alerts) {
-//       print('${alert.fromNickname} 님이 ${alert.alertType} 하셨습니다');
-//     }
-//   } catch (e) {
-//     print('에러입니다: $e');
-//   }
-// }
-
-
-
-
 class ListApp extends StatelessWidget {
   const ListApp({Key? key}) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +20,9 @@ class ListApp extends StatelessWidget {
   }
 }
 
-
 class ArletListView extends StatefulWidget {
-  const ArletListView({super.key});
-  static const route = '/arlet-screen';
+  const ArletListView({Key? key}) : super(key: key);
+  static const route = '/alerts';
 
   @override
   State<ArletListView> createState() => _ArletListViewState();
@@ -53,9 +32,7 @@ class _ArletListViewState extends State<ArletListView> {
   final dio = Dio();
   final fcmapiService = FcmApiService(Dio());
 
-  var fromNickname = [];
-  var titleList = [];
-  var alertTypeList = [];
+  List<ApiArletModel> alertData = []; // 변경된 데이터 타입
 
   @override
   void initState() {
@@ -65,124 +42,126 @@ class _ArletListViewState extends State<ArletListView> {
 
   // API 요청을 처리하고 데이터를 할당하는 함수
   Future<void> fetchData() async {
-
-
     try {
-      final alerts = await fcmapiService.getPosts(); // API로부터 데이터 가져오기
-
+      final response = await fcmapiService.getPosts(); // List<ApiArletModel> 형식의 데이터 가져오기
+      print('API 응답: $response'); // 응답 데이터 출력
+      final alerts = response; // 실제 데이터 추출
       setState(() {
-        // 받아온 데이터를 변수에 할당
-        fromNickname = alerts.map((alert) => alert.fromNickname).toList();
-        titleList = alerts.map((alert) => alert.title).toList();
-        alertTypeList = alerts.map((alert) => alert.alertType).toList();
+        alertData = alerts; // 그대로 할당
+        print('Fetching data...');
       });
     } catch (e) {
-      print('에러입니다: $e');
+      print('에러입니다 여기에요!! 여기로 들어와요!!: $e');
     }
   }
 
-
   void showPopup(context, fromNickname, titleList, alertTypeList) {
-    showDialog(context: context, builder: (context) {
-      return Dialog(
-        child: Container(
-          width: MediaQuery.of(context).size.width*0.7 ,
-          height: 380,
-          decoration: BoxDecoration(
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 380,
+            decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: Colors.white
-          ),
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Text(fromNickname, style: TextStyle(
-                  fontSize: 20,
-                ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                alertTypeList,
-                style: const TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(padding: EdgeInsets.all(8),
-                child: Text(
-                  titleList,
-                  maxLines: 6,
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Text(
+                    fromNickname ?? "알림없음",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  alertTypeList ?? "알림없음",
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    titleList ?? "알림없음",
+                    maxLines: 6,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
-
-    final message = ModalRoute.of(context)!.settings.arguments;
-
     return Scaffold(
-
       appBar: TitleBar(
         appBar: AppBar(),
         title: ' 알림함',
         actions: [],
-        leading: null,),
+        leading: null,
+      ),
       body: Container(
         color: LIST_BLACK_COLOR,
-        padding: EdgeInsets.all(16), // 간격 조정
+        padding: EdgeInsets.all(16),
         child: ListView.builder(
-          itemCount: titleList.length,
+          itemCount: alertData.length,
           itemBuilder: (context, index) {
             return GestureDetector(
               onTap: () {
-                debugPrint(alertTypeList[index]);
-                showPopup(context, fromNickname[index], titleList[index], alertTypeList[index]);
+                if (alertData.isEmpty) {
+                  return;
+                }
+                debugPrint(alertData[index].alertType);
+                showPopup(
+                  context,
+                  alertData[index].fromNickname,
+                  alertData[index].title,
+                  alertData[index].alertType,
+                );
               },
               child: Card(
                 color: Colors.black,
-
                 child: Row(
                   children: [
                     SizedBox(
                       width: 20,
                       height: 80,
-
-
                     ),
-                    Padding(padding: EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [Text('${fromNickname[index]} 님이 ${alertTypeList[index]} 하셨습니다',
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${alertData[index].fromNickname} 님이 ${alertData[index].alertType} 하셨습니다',
                             style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
                           ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                          ],
-                        )
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
                     )
                   ],
                 ),
