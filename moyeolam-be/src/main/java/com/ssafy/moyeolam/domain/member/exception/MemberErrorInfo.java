fac4d51a8d5f25539e5d0eb284b02452ef1a20d2
jjuny7712 <jjuny7712@gmail.com>
2023-08-09 17:52:38 +0900

    fix: 사용자 검색 로직 수정

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/member/exception/MemberErrorInfo.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/member/exception/MemberErrorInfo.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum MemberErrorInfo {
     NOT_FOUND_MEMBER("601","멤버를 찾을 수 없습니다."),
     NOT_FOUND_PROFILEIMAGE("602", "프로필 이미지를 찾을 수 없습니다."),
-    NICKNAME_ALREADY_IN_USE("603", "이미 사용중인 닉네임 입니다.");
+    NICKNAME_ALREADY_IN_USE("603", "이미 사용중인 닉네임 입니다."),
+    NOT_FOUND_MEMBER_BY_NICKNAME("604", "해당 닉네임의 사용자를 찾을 수 없습니다.");
 
 
     private String code;
