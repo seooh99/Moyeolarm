388e1dd54605749d0309b5f17399e2ae1556f430
최휘빈 <chlgnlqls3@nate.com>
2023-08-10 10:29:49 +0900

    Feature/friends search logic #246
    * feat: 친구검색 로직 구현
    
    * feat: 친구목록 로직 구현

---
+++ moyeolam-fe/lib/friends/provider/friends_search_provider.dart
@@ -0,0 +1,28 @@
+// final alarmListProvider = FutureProvider<List<AlarmGroups>?>((ref) async {
+//   final dio = Dio();
+//   final alarms = AlarmListRepository(dio);
+//
+//   return await alarms.getAlarmList().then((value) => value.data.alarmGroups);
+// });
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youngjun/friends/model/friends_search_model.dart';
+import '../repository/friends_repository.dart';
+
+final friendsSearchProvider = FutureProvider<List<Friend>?>((ref) async {
+  final dio = Dio();
+  final friends = FriendsRepository(dio);
+
+  final friendsSearchModel =
+      await friends.searchFriends('').catchError((error) {
+    // 에러 처리 로직 추가
+    print('Error: $error');
+    return FriendsSearchModel(
+        code: '',
+        message: '',
+        data: Data(friends: [])); // 에러 발생 시 빈 모델 반환 또는 알맞은 처리
+  });
+
+  return friendsSearchModel.data.friends;
+});
