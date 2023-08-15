a36d48260fd2e32b8341c4c6c406af2d997a1040
jjuny7712 <jjuny7712@gmail.com>
2023-08-15 11:57:31 +0900

    fix: 로그인 시 memberId 응답 추가

--- moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/dto/LoginResponseDto.java
+++ moyeolam-be/src/main/java/com/ssafy/moyeolam/domain/auth/dto/LoginResponseDto.java
@@ -15,13 +15,15 @@ import java.util.stream.Collectors;
 @Getter
 @Builder
 public class LoginResponseDto {
+    private Long memberId;
     private String nickname;
     private String profileImageUrl;
     private String accessToken;
     private String refreshToken;
 
-    public static LoginResponseDto of(Member member, ProfileImage profileImage, String accessToken, String refreshToken){
+    public static LoginResponseDto of(Member member, ProfileImage profileImage, String accessToken, String refreshToken) {
         return LoginResponseDto.builder()
+                .memberId(member.getId())
                 .nickname(member.getNickname())
                 .profileImageUrl(profileImage.getImageUrl())
                 .accessToken(accessToken)
